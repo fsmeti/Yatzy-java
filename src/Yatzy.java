13bc3a3bca0593305bc6deb1f10e7d9de1ddfd6f
mca3830 <mylena.cauche@viseo.com>
2018-12-12 20:01:16 +0100

    refactoring all the methods excluding full house

--- src/Yatzy.java
+++ src/Yatzy.java
@@ -1,195 +1,138 @@
+import java.util.Arrays;
+
 public class Yatzy {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
-    {
+    public static int chance(int d1, int d2, int d3, int d4, int d5) {
         return d1 + d2 + d3 + d4 + d5;
     }
 
-    public static int yatzy(int... dice)
-    {
-        int[] counts = new int[6];
-        for (int die : dice)
-            counts[die-1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
-                return 50;
-        return 0;
+    public static int yatzy(int... dice) {
+        int counts = 0;
+        int diceValue = 0;
+        for (int die : dice){
+            if (diceValue == 0 || diceValue == die){
+                diceValue = die;
+                counts++;
+            }
+        }
+        return (counts == 5)? 50 : 0;
     }
 
-    public static int countOccurrencesOf(int number, int[] roll){
+    public static int countOccurrence(int number, int[] roll) {
         int occurrencesNumber = 0;
-        for (int i = 0; i < roll.length; i++) {
+        for (int i = 0; i < roll.length; i++)
             if (number == roll[i]) occurrencesNumber++;
-        }
         return occurrencesNumber;
     }
 
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
         int[] roll = {d1, d2, d3, d4, d5} ;
-        int onesOccurrences = Yatzy.countOccurrencesOf(1, roll);
+        int onesOccurrences = Yatzy.countOccurrence(1, roll);
         return onesOccurrences;
     }
 
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
         int[] roll = {d1, d2, d3, d4, d5} ;
-        int twosOccurrences = Yatzy.countOccurrencesOf(2, roll);
+        int twosOccurrences = Yatzy.countOccurrence(2, roll);
         return twosOccurrences * 2;
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;    
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
+        int[] roll = {d1, d2, d3, d4, d5} ;
+        int threeOccurrences = Yatzy.countOccurrence(3, roll);
+        return threeOccurrences * 3;
     }
 
     protected int[] dice;
-    public Yatzy(int d1, int d2, int d3, int d4, int _5)
-    {
+    public Yatzy(int d1, int d2, int d3, int d4, int d5) {
         dice = new int[5];
         dice[0] = d1;
         dice[1] = d2;
         dice[2] = d3;
         dice[3] = d4;
-        dice[4] = _5;
+        dice[4] = d5;
     }
 
-    public int fours()
-    {
-        int sum;    
-        sum = 0;
-        for (int at = 0; at != 5; at++) {
-            if (dice[at] == 4) {
-                sum += 4;
-            }
-        }
-        return sum;
+    public int fours() {
+        return Yatzy.countOccurrence(4, dice) * 4;
     }
 
-    public int fives()
-    {
-        int s = 0;
-        int i;
-        for (i = 0; i < dice.length; i++) 
-            if (dice[i] == 5)
-                s = s + 5;
-        return s;
+    public int fives() {
+        return Yatzy.countOccurrence(5, dice) * 5;
     }
 
-    public int sixes()
-    {
-        int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
-            if (dice[at] == 6)
-                sum = sum + 6;
-        return sum;
+    public int sixes() {
+        return Yatzy.countOccurrence(6, dice) * 6;
+    }
+
+    public static int findCategory(int number, int[] roll, int occurrenceCategory){
+        int occurrence = Yatzy.countOccurrence(number, roll);
+        return (occurrence >= occurrenceCategory)? number * occurrenceCategory : 0;
     }
 
     public static int score_pair(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6-at-1] >= 2)
-                return (6-at)*2;
-        return 0;
+        int score = 0;
+        int[] roll = {d1, d2, d3, d4, d5};
+        for(int i=6; i>=1; i--){
+            score = Yatzy.findCategory(i, roll, Category.PAIR.getValue());
+            if(score > 0) return score;
+        }
+        return score;
     }
 
     public static int two_pair(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int n = 0;
         int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6-i-1] >= 2) {
-                n++;
-                score += (6-i);
-            }        
-        if (n == 2)
-            return score * 2;
-        else
-            return 0;
+        int pairOccurrence = 0;
+        int[] roll = {d1, d2, d3, d4, d5};
+        for(int i=6; i>=1; i--){
+            int pairScore = Yatzy.findCategory(i, roll, Category.PAIR.getValue());
+            if(pairScore > 0) {
+                score += pairScore;
+                pairOccurrence++;
+            }
+        }
+        return (pairOccurrence == 2)? score : 0;
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
+    public static int four_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
+        int score = 0;
+        int[] roll = {d1, d2, d3, d4, d5};
+        for(int i=1; i<=6; i++){
+            score += Yatzy.findCategory(i, roll, Category.FOUR_OF_A_KIND.getValue());
+        }
+        return score;
     }
 
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i+1) * 3;
-        return 0;
+    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
+        int score = 0;
+        int[] roll = {d1, d2, d3, d4, d5};
+        for(int i=1; i<=6; i++){
+            score += Yatzy.findCategory(i, roll, Category.THREE_OF_A_KIND.getValue());
+        }
+        return score;
     }
 
-    public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
-            return 15;
-        return 0;
+    public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
+        int score = 0;
+        int[] roll = {d1, d2, d3, d4, d5};
+        int[] smallStraight = {1, 2, 3, 4, 5};
+        Arrays.sort(roll);
+        if(Arrays.equals(smallStraight, roll))
+            score = 15;
+        return score;
     }
 
-    public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
-        return 0;
+    public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
+        int score = 0;
+        int[] roll = {d1, d2, d3, d4, d5};
+        int[] largeStraight = {2, 3, 4, 5, 6};
+        Arrays.sort(roll);
+        if(Arrays.equals(largeStraight, roll))
+            score = 20;
+        return score;
     }
 
     public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
@@ -200,9 +143,6 @@ public class Yatzy {
         int _2_at = 0;
         boolean _3 = false;
         int _3_at = 0;
-
-
-
 
         tallies = new int[6];
         tallies[d1-1] += 1;
@@ -229,6 +169,3 @@ public class Yatzy {
             return 0;
     }
 }
-
-
-
